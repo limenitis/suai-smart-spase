760ce5f7bf93efb5ed8e0f023b4c8633b9579e43
limenitis <ini.ivi@yandex.ru>
2021-02-19 00:15:08 +0300

    fix tests

--- entrance-test-tests/test.cpp
+++ entrance-test-tests/test.cpp
@@ -1,103 +1,69 @@
 #include "pch.h"
 #include ".\..\entrance-test\functions.c"
-
+#include <string>
 
 namespace str_tests{
 
-	// 	dddddd
-	char Simple_Test_str[] = { 'd', 'd', 'd', 'd', 'd', 'd', '\0'};
-	
-	// dd ss aa rr rrw
-	char Words_In_Start_str[] = { 'd', 'd', ' ', 's', 's', ' ', 'a', 'a', ' ', 'r', 'r', ' ', 'r', 'r', 'w', '\0'};
-	
-	// sggs aasfa gs r gsg
-	char Words_In_Middle_str[] = { 's', 'g', 'g', 's', ' ', 'a', 'a', 's', 'f', 'a', ' ', 'g', 's', ' ', 'r', ' ', 'g', 's', 'g', '\0'};
-	
-	// tjs dsDGER asfs teer ew d trref
-	char Words_Few_str[] = { 't', 'j', 's', ' ', 'd', 's', 'D', 'G', 'E', 'R', ' ', 'a', 's', 'f', 's', ' ', 't', 'e', 'e', 'r', ' ', 'e', 'w', ' ', 'd', ' ', 't', 'r', 'r', 'e', 'f', '\0'};
-	
-	// asf hrtf v sfrewer f dssa r
-	char Words_In_End_str[] = { 'a', 's', 'f', ' ', 'h', 'r', 't', 'f', ' ', 'v', ' ', 's', 'f', 'r', 'e', 'w', 'e', 'r', ' ', 'f', ' ', 'd', 's', 's', 'a', ' ', 'r', '\0' };
-	
-	//       rewe dasda rbtr zxcr
-	char Spaces_In_Starts_str[] = { ' ', ' ', ' ', ' ', ' ', ' ', 'r', 'e', 'w', 'e', ' ', 'd', 'a', 's', 'd', 'a', ' ', 'r', 'b', 't', 'r', ' ', 'z', 'x', 'c', 'r', '\0' };
-	
-	// rege    qwdwqd wqwqd     fetgr
-	char Spaces_In_Middle_str[] = { 'r', 'e', 'g', 'e', ' ', ' ', ' ', ' ', 'q', 'w', 'd', 'w', 'q', 'd', ' ', 'w', 'q', 'w', 'q', 'd', ' ', ' ', ' ', ' ', ' ', 'f', 'e', 't', 'g', 'r', '\0'};
-	
-	// saf ryhrw f sdr4    dqdq         
-	char Spaces_In_End_str[] = { 's', 'a', 'f', ' ', 'r', 'y', 'h', 'r', 'w', ' ', 'f', ' ', 's', 'd', 'r', '4', ' ', ' ', ' ', ' ', 'd', 'q', 'd', 'q', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '\0' };
-	
-	// sad#$!d d#4$12@ f#@525#& &*#@! d$12#
-	char Consist_Special_Symbols_str[] = { 's', 'a', 'd', '#', '$', '!', 'd', ' ', 'd', '#', '4', '$', '1', '2', '@', ' ', 'f', '#', '@', '5', '2', '5', '#', '&', ' ', '&', '*', '#', '@', '!', ' ', 'd', '$', '1', '2', '#', '\0'};
-
-	// \0
-	char Empty_Str_str[] = {'\0'};
-
-	// s
-	char One_Symbol_With_Return_Str_str[] = {'s', '\0'};
-
-	// d
-	char One_Symbol_Without_Return_Str_str[] = {'d', '\0'};
+	std::string str = "";
 
 	TEST(TestStrFunctions, Simple_Test) {
-			
-		EXPECT_STREQ("dddddd ", str_processing(Simple_Test_str, 'd'));
+		str = "dddddd";
+		EXPECT_STREQ("dddddd ", str_processing(const_cast<char*>(str.c_str()), 'd'));
 	}
 
 	TEST(TestStrFunctions, Words_In_Start) {
-
-		EXPECT_STREQ("dd ", str_processing(Words_In_Start_str, 'd'));
+		str = "dd ss aa rr rrw	";
+		EXPECT_STREQ("dd ", str_processing(const_cast<char*>(str.c_str()), 'd'));
 	}
 
 	TEST(TestStrFunctions, Words_In_Middle) {
-
-		EXPECT_STREQ("aasfa ", str_processing(Words_In_Middle_str, 'a'));
+		str = "sggs aasfa gs r gsg";
+		EXPECT_STREQ("aasfa ", str_processing(const_cast<char*>(str.c_str()), 'a'));
 	}
 
 	TEST(TestStrFunctions, Words_Few) {
-
-		EXPECT_STREQ("tjs teer trref ", str_processing(Words_Few_str, 't'));
+		str = "tjs dsDGER asfs teer ew d trref";
+		EXPECT_STREQ("tjs teer trref ", str_processing(const_cast<char*>(str.c_str()), 't'));
 	}
 
 	TEST(TestStrFunctions, Words_In_End) {
-
-		EXPECT_STREQ("r ", str_processing(Words_In_End_str, 'r'));
+		str = "asf hrtf v sfrewer f dssa r";
+		EXPECT_STREQ("r ", str_processing(const_cast<char*>(str.c_str()), 'r'));
 	}
 
 	TEST(TestStrFunctions, Spaces_In_Starts) {
-
-		EXPECT_STREQ("dasda ", str_processing(Spaces_In_Starts_str, 'd'));
+		str = "      rewe dasda rbtr zxcr";
+		EXPECT_STREQ("dasda ", str_processing(const_cast<char*>(str.c_str()), 'd'));
 	}
 
 	TEST(TestStrFunctions, Spaces_In_Middle) {
-
-		EXPECT_STREQ("wqwqd ", str_processing(Spaces_In_Middle_str, 'w'));
+		str = "rege    qwdwqd wqwqd     fetgr";
+		EXPECT_STREQ("wqwqd ", str_processing(const_cast<char*>(str.c_str()), 'w'));
 	}
 
 	TEST(TestStrFunctions, Spaces_In_End) {
-
-		EXPECT_STREQ("f ", str_processing(Spaces_In_End_str, 'f'));
+		str = "saf ryhrw f sdr4    dqdq         ";
+		EXPECT_STREQ("f ", str_processing(const_cast<char*>(str.c_str()), 'f'));
 	}
 
 	TEST(TestStrFunctions, Consist_Special_Symbols) {
-
-		EXPECT_STREQ("d#4$12@ d$12# ", str_processing(Consist_Special_Symbols_str, 'd'));
+		str = "sad#$!d d#4$12@ f#@525#& &*#@! d$12#";
+		EXPECT_STREQ("d#4$12@ d$12# ", str_processing(const_cast<char*>(str.c_str()), 'd'));
 	}
 
 	TEST(TestStrFunctions, Empty_Str) {
-
-		EXPECT_STREQ("", str_processing(Empty_Str_str, 's'));
+		str = "\0";
+		EXPECT_STREQ("", str_processing(const_cast<char*>(str.c_str()), 's'));
 	}
 
 	TEST(TestStrFunctions, One_Symbol_With_Return_Str) {
-
-		EXPECT_STREQ("s ", str_processing(One_Symbol_With_Return_Str_str, 's'));
+		str = "s";
+		EXPECT_STREQ("s ", str_processing(const_cast<char*>(str.c_str()), 's'));
 	}
 
 	TEST(TestStrFunctions, One_Symbol_Without_Return_Str) {
-
-		EXPECT_STREQ("", str_processing(One_Symbol_Without_Return_Str_str, 's'));
+		str = "d";
+		EXPECT_STREQ("", str_processing(const_cast<char*>(str.c_str()), 's'));
 	}
 }
 
